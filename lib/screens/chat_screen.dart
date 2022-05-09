7415f4d899cd3f6bc9fb49a9359c83842ac47dd8
Sarvagya Verma <sarvagya1115@gmail.com>
2022-05-09 23:56:21 +0530

    Logging current user

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -11,9 +11,24 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   final _auth = FirebaseAuth.instance;
-  Firebase loggedInUser;
-  void getCurrentUser() async {
-    loggedInUser = await _auth.currentUser;
+  User loggedInUser;
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUser();
+  }
+
+  void getCurrentUser() {
+    try {
+      final user = _auth.currentUser;
+      if (user != null) {
+        loggedInUser = user;
+        print(loggedInUser.email);
+      }
+    } catch (e) {
+      print(e);
+    }
   }
 
   @override
